abfad425e4536cb8a03abfa6e89c1ee8258b1359
anvisingh12 <anvi3rcs@gmail.com>
2024-04-20 09:22:21 -0700

    fixed PID controls

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,27 +1,36 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 
-import edu.wpi.first.math.controller.PIDController;
+// import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterSubsystem extends SubsystemBase {
   
     private TalonFX shootermotor1; 
-    private PIDController pidController_;
+    // private PIDController pidController_;
 
   public ShooterSubsystem() {
     shootermotor1 = new TalonFX(0);
-    pidController_ = new PIDController(0,0,0,0);
-  }
+    // pidController_ = new PIDController(0,0,0,0);
+    // in init function, set slot 0 gains
+    var slot0Configs = new Slot0Configs();
+    slot0Configs.kS = 0.05; // Add 0.05 V output to overcome static friction
+    slot0Configs.kV = 0.12; // A velocity target of 1 rps results in 0.12 V output
+    slot0Configs.kP = 0.11; // An error of 1 rps results in 0.11 V output
+    slot0Configs.kI = 0; // no output for integrated error
+    slot0Configs.kD = 0; // no output for error derivative
 
-
-  public void setPID(double kp, double ki, double kd) {
-    pidController_.setPID(kp, ki, kd);
+    shootermotor1.getConfigurator().apply(slot0Configs);
   }
+  // // setting PID values 
+  // public void setPID(double kp, double ki, double kd) {
+  //   pidController_.setPID(kp, ki, kd);
+  // }
 
   // start shooter and set to certain rps 
   public void startShooter(double rps){
